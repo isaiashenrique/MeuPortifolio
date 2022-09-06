57a43eac18c9e6753b5e397311f05973e3e49399
isaias henrique <isaiashenriquee@gmail.com>
2022-09-05 22:16:57 -0300

    parte final da criação do portifolio

--- assets/js/habilidades.js
+++ assets/js/habilidades.js
@@ -3,7 +3,7 @@ const descricao = document.querySelector('.texto-descricao');
 const sobreSkill = [
                     '<p>HTML</p> <br> <p>É uma linguagem de marcação utilizada na construção de páginas na Web.</p> <br>',
                     '<p>CSS</p> <br> <p>É um mecanismo para adicionar estilo a um documento web.</p> <br>',
-                    '<p>JavaScript</p> <br> <p>É uma linguagem de programação. Juntamente com HTML e CSS, é uma das três principais tecnologias da web.</p> <br>',
+                    '<p>JavaScript</p> <br> <p>É a linguagem de programação usada para adicionar interatividade ao seu site</p> <br>',
                     '<p>Git</p> <br> <p>É um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software.</p> <br>',
                     '<p>Github</p> <br> <p>É uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. </p> <br>',
                     '<p>ReactJS</p> <br> <p>O React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.</p> <br>',
@@ -33,7 +33,7 @@ skills.forEach(  (elemento, index) => {
         descricao.innerHTML = `<p>${sobreSkill[index1]} </p>` ;
     } )
     elemento.addEventListener('mouseout', (evento, elemento,) => {
-        descricao.innerHTML = ' *Passe o mouse por cima de alguma habilidade para ler a descrição.*';
+        descricao.innerHTML = ' Passe o mouse por cima de alguma habilidade para ler a descrição.';
     } )
 } );
 
